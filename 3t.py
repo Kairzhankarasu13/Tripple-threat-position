01fec2748e8047a319baba9863f50f7fa7ba385f
kairzhan <13@gmail.com>
2021-09-28 21:22:47 +0600

    '3'

--- 3t.py
+++ 3t.py
@@ -7,9 +7,21 @@ step1=int(input("Вариант: "))
 if step1==1:
     print("1. Пас на дугу\n2. Пас на кольцо")
     step2=int(input("\nВариант: "))
+    if step2==1:
+        print("\nТрех очковый бросок")
+    elif step2==2:
+        print("\nAlley-oop")
 elif step1==2:
     print("1. Бросок с места\n2. Бросок с отклонением назад")
     step2=int(input("\nВариант: "))
+    if step2==1:
+        print("\nБросок заблокировали")
+    elif step2==2:
+        print("\nДвух очковый бросок от щита")
 elif step1==3:
     print("1. Пройти с вращением\n2. Обманный бросок")
     step2=int(input("\nВариант: "))
+    if step2==1:
+        print("\nПерехват мяча")
+    elif step2==2:
+        print("\nПроход под кольцо и данк")
